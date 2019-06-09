d537d5e41db9e7b64ffafca310d5c6eb4ee49cda
Sam Maxwell <smaxwellmessages@gmail.com>
2019-06-09 12:23:30 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -27,19 +27,31 @@ function checkForMatch(){
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again");
-	}		
+	}	
+	cardsInPlay.pop();
+	cardsInPlay.pop();
+		
 }
 
-function flipCard(cardId){
+function flipCard(){
+	var cardId=this.getAttribute('data-id');
 	var flippedCard = cards[cardId];
 	console.log("User flipped " + flippedCard.rank + " of " + flippedCard.suit + " " + flippedCard.cardImage);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src',flippedCard.cardImage);
 	if(cardsInPlay.length === 2){
 		checkForMatch();
 	}
 	
 }
-
-flipCard(0);
-flipCard(2);
+function createBoard(){
+	for(i=0; i<cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id',i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+createBoard();
 
